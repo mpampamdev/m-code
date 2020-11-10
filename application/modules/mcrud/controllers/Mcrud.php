ceefc5300e3dbafcf4418cb5c8d42aa8bc88c4c9
mpampamdev <muhirfan.teknik@gmail.com>
2020-11-10 13:34:44 +0700

    v1

--- application/modules/mcrud/controllers/Mcrud.php
+++ application/modules/mcrud/controllers/Mcrud.php
@@ -50,8 +50,8 @@ class Mcrud extends CI_Controller{
     $json = array('success' => false, 'msg' => array());
     $this->form_validation->set_rules("table","Table","trim|xss_clean|required");
     $this->form_validation->set_rules("title","Title","trim|xss_clean|required");
-    $this->form_validation->set_rules("model","Model","trim|xss_clean|required|alpha_underscores");
-    $this->form_validation->set_rules("controller","Controller","trim|xss_clean|alpha_underscores|required");
+    $this->form_validation->set_rules("model","Model","trim|xss_clean|required|alpha_underscores|callback__cek_model");
+    $this->form_validation->set_rules("controller","Controller","trim|xss_clean|alpha_underscores|required|_cek_controller");
     $this->form_validation->set_rules("show_table[]","Show on table","trim|xss_clean|required",[
       "required" => "(".cclang('show_in_table').") ".cclang("select_one_field")
     ]);
