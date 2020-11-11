83a099a20b9eb05eb00c8e72266e9539334c14a9
mpampamdev <muhirfan.teknik@gmail.com>
2020-11-12 03:14:51 +0700

    v1

--- application/modules/backend/models/Setting_model.php
+++ application/modules/backend/models/Setting_model.php
@@ -1,6 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+/* dev : mpampam*/
+/* fb : https://facebook.com/mpampam*/
+/* fanspage : https://web.facebook.com/programmerjalanan*/
+/* web : www.mpampam.com*/
+/* Generate By M-CRUD Generator 10/11/2020 14:51*/
+/* Please DO NOT modify this information */
 class Setting_model extends MY_Model{
 
   public function __construct()
